575ccda4e306e28513a6acfb3c4d393dd169302e
Gourab Porey <gourabporey@users.noreply.github.com>
2023-06-05 12:36:00 +0530

    Add: `MyEventEmitter` to replicate the `on` and `emit` method of emitter

--- jun/events/eventEmitter.js
+++ jun/events/eventEmitter.js
@@ -1,9 +1,29 @@
-const EventEmitter = require('events');
+// const EventEmitter = require('events');
 
-const em = new EventEmitter();
+// const em = new EventEmitter();
 
-em.on('start', () => console.log('started'));
-em.emit('start');
+// em.on('start', () => console.log('started'));
+// em.emit('start');
 
-em.on('done', (data) => console.log(data));
-em.emit('done', 'The event is done executing');
+// em.on('done', (data) => console.log(data));
+// em.emit('done', 'The event is done executing');
+
+class MyEventEmitter {
+  constructor() {
+    this.subscribers = {};
+  }
+
+  on(event, callback) {
+    this.subscribers[event] = (this.subscribers[event] || []).concat(callback);
+  }
+
+  emit(event, data) {
+    this.subscribers[event].forEach(subscriber => {
+      subscriber(data);
+    });
+  }
+}
+
+const myEventEmitter = new MyEventEmitter();
+myEventEmitter.on('start', () => console.log('started'));
+myEventEmitter.emit('start');
