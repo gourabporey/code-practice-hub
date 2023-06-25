34b2dfcaad636655e57a2da932fa01f3dd157115
Gourab Porey <gourabporey@users.noreply.github.com>
2023-06-25 21:43:01 +0530

    refactor: change `calculator REPLTest.js` using node test mocking
    This commit refactors the REPLTest.js file to improve readability and maintainability. The changes include:
    
    - Updating test cases to use the provided context object for mocking and assertions.
    - Adjusting assertions to use the mock object from the context for better tracking of function calls.

--- jun/calculator-with-dependency-injection/test/REPLTest.js
+++ jun/calculator-with-dependency-injection/test/REPLTest.js
@@ -1,43 +1,46 @@
-const { describe, it } = require("node:test");
-const { REPL } = require("../src/repl");
-const { createSpyFunction } = require("./spyLib");
 const assert = require("assert");
+const { describe, it } = require("node:test");
+
+const { REPL } = require("../src/repl");
 const { Calculator } = require("../src/calculator");
 
 describe('REPL', () => {
   describe('run', () => {
-    it('starts watching the new lines on inputstream', () => {
-      const io = { watchInputLine: createSpyFunction() };
+    it('starts watching the new lines on inputstream', (context) => {
+      const io = { watchInputLine: context.mock.fn() };
       const repl = new REPL(io, null);
       repl.run();
-      assert.ok(io.watchInputLine.wasCalledOnce());
-    })
-  })
-  
+      assert.strictEqual(io.watchInputLine.mock.callCount(), 1);
+    });
+  });
+
   describe('processLine', () => {
-    it('writes available options when invalid operator is provided', () => {
-      const io = { writeLine: createSpyFunction() };
+    it('writes available options when invalid operator is provided', (context) => {
+      const io = {
+        watchInputLine: context.mock.fn(),
+        writeLine: context.mock.fn()
+      };
       const calc = { validOperators: ['one', 'two'] };
       const repl = new REPL(io, calc);
       repl.processLine('bad 2');
-      assert.ok(io.writeLine.wasCalledOnce('one two'))
-    })
+      assert.deepStrictEqual(io.writeLine.mock.calls[0].arguments, ['one two']);
+    });
 
-    it('writes available options when invalid operand is provided', () => {
-      const io = { writeLine: createSpyFunction() };
+    it('writes available options when invalid operand is provided', (context) => {
+      const io = { writeLine: context.mock.fn() };
       const calc = new Calculator();
       const repl = new REPL(io, calc);
       repl.processLine('one abc');
-      assert.ok(io.writeLine.wasCalledOnce('add sub'))
-    })
+      assert.deepStrictEqual(io.writeLine.mock.calls[0].arguments, ['add sub']);
+    });
 
-    it('writes balance after operating when valid operand and operators are provided', () => {
-      const io = { writeLine: createSpyFunction() };
+    it('writes balance after operating when valid operand and operators are provided', (context) => {
+      const io = { writeLine: context.mock.fn() };
       const calc = new Calculator();
       const repl = new REPL(io, calc);
       repl.processLine('add 1');
 
-      assert.ok(io.writeLine.wasCalledOnce(1));
-    })
-  })
+      assert.deepStrictEqual(io.writeLine.mock.calls[0].arguments, [1]);
+    });
+  });
 });
