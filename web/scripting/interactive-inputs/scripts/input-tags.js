a32693b6f48e8daac6e65b8013e34f8979bd3c0f
Gourab Porey <gourabporey@users.noreply.github.com>
2023-07-22 14:41:13 +0530

    add option selection of countries

--- web/scripting/interactive-inputs/scripts/input-tags.js
+++ web/scripting/interactive-inputs/scripts/input-tags.js
@@ -1,12 +1,36 @@
 const getInputBox = () => document.querySelector('#comment');
 
-window.onload = () => {
-  const inputBox = getInputBox();
-
+const getInputOnEnter = (inputBox, getTextOfInputBox) => {
   inputBox.addEventListener('keyup', (event) => {
     if (event.key === 'Enter') {
-      console.log(inputBox.value);
+      getTextOfInputBox(inputBox.value);
       inputBox.value = '';
     }
   });
+};
+
+const getInputOnChange = (textArea, getText) => {
+  textArea.onchange = getText;
+};
+
+const getOptionValue = (selectionGroup, getSelected) => {
+  selectionGroup.onchange = () => {
+    getSelected(selectionGroup.value);
+  };
+};
+
+const printText = (text) => {
+  console.log(text);
+};
+
+window.onload = () => {
+  const inputBox = getInputBox();
+  getInputOnEnter(inputBox, printText);
+
+  const textArea = document.querySelector('#comment-area');
+  getInputOnEnter(textArea, printText);
+  getInputOnChange(textArea, printText);
+
+  const countrySelection = document.querySelector('#select-country');
+  getOptionValue(countrySelection, printText);
 };
