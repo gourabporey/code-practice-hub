e7544254815a21656fc020ed784ea9f0a319b99a
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-05 12:54:19 +0530

    add response based on uri practice

--- javascript/http/server.js
+++ javascript/http/server.js
@@ -1,17 +1,40 @@
 const net = require('node:net');
 
-const server = net.createServer();
+const getContent = (uri) => {
+  const contents = {
+    '/': { content: 'home', statusCode: 200, status: 'OK' },
+    '/ping': { content: 'pong', statusCode: 200, status: 'OK' },
+    '/echo': { content: 'echo', statusCode: 200, status: 'OK' },
+  };
 
-const html = `Hello World`;
+  const notFoundContent = {
+    statusCode: 404,
+    status: 'Not Found',
+    content: 'Not Found',
+  };
 
-const response = `HTTP 410 \n\n${html}`;
+  return contents[uri] || notFoundContent;
+};
 
-server.on('connection', (socket) => {
-  socket.setEncoding('utf-8').on('data', (data) => {
-    console.log(data);
-    socket.write(response);
-    socket.end();
+const generateResponse = (status, statusCode, content) =>
+  `HTTP/1.1 ${statusCode} ${status}\n\n ${content}`;
+
+const main = () => {
+  const server = net.createServer();
+
+  server.on('connection', (socket) => {
+    socket.setEncoding('utf-8').on('data', (req) => {
+      const [, uri] = req.split(' ');
+      const { content, statusCode, status } = getContent(uri);
+      const response = generateResponse(status, statusCode, content);
+
+      socket.write(response);
+      socket.end();
+    });
   });
-});
 
-server.listen(8000, () => console.log('Listening on', 8000));
+  const PORT = 8000;
+  server.listen(PORT, () => console.log('Listening on', PORT));
+};
+
+main();
