dca4f0624c0f03070ebc663cf90a5c7699d3f3ad
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-05 16:19:40 +0530

    add echo method to show path following it

--- javascript/http/server.js
+++ javascript/http/server.js
@@ -1,23 +1,37 @@
 const net = require('node:net');
 
+const STATUS = {
+  200: { code: 200, message: 'OK' },
+  404: { message: 'Not Found', code: 404 },
+};
+
+const isEcho = (uri) => uri.startsWith('/echo/');
+
+const getEchoResponse = (uri) => {
+  const content = uri.replace('/echo/', '');
+  const status = STATUS[200];
+
+  return { content, status };
+};
+
 const getContent = (uri) => {
+  if (isEcho(uri)) return getEchoResponse(uri);
+
   const responses = {
-    '/': { content: 'home', status: { code: 200, message: 'OK' } },
-    '/ping': { content: 'pong', status: { code: 200, message: 'OK' } },
-    '/echo': { content: 'echo', status: { code: 200, message: 'OK' } },
+    '/': { content: 'home', status: STATUS[200] },
+    '/ping': { content: 'pong', status: STATUS[200] },
+    '/echo': { content: 'echo', status: STATUS[200] },
   };
 
-  const uriNotFound = {
-    status: { message: 'Not Found', code: 404 },
-    content: `${uri} Not Found`,
-  };
+  const uriNotFound = { status: STATUS[404], content: `${uri} Not Found` };
 
   return responses[uri] || uriNotFound;
 };
 
 const parseRequest = (requestData) => {
   const [requestLine] = requestData.split('\n');
-  const [verb, uri, protocol] = requestLine.split(' ');
+  const [verb, uri, protocol] = requestLine.trim().split(' ');
+
   return { verb, uri, protocol };
 };
 
@@ -27,34 +41,30 @@ const generateResponse = ({ status, content }) => {
   return `${PROTOCOL} ${status.code} ${status.message}\n\n${content}`;
 };
 
-const isInvalidProtocol = (protocol) => {
-  return !protocol || protocol.trim().toUpperCase() !== PROTOCOL;
-};
+const isInvalidProtocol = (protocol) =>
+  !protocol || protocol.toUpperCase() !== PROTOCOL;
 
 const isInvalidVerb = (verb) => verb.toUpperCase() !== 'GET';
 
-const getBadReqResponse = () => {
-  const status = { code: 400, message: 'Bad Request' };
-  const content = 'Bad Request';
+const getProtocolErrResponse = () => {
+  const content = 'BAD_REQUEST';
+  const status = { code: 400, message: content };
 
   return generateResponse({ status, content });
 };
 
-const getContentReqResponse = (uri) => {
-  const { content, status } = getContent(uri);
-  return generateResponse({ status, content });
-};
-
-const getBadVerbResponse = (verb) => {
-  const content = `${verb} Not Allowed`;
+const getBadVerbErrResponse = () => {
+  const content = `METHOD_NOT_ALLOWED`;
   const status = { code: 405, message: content };
 
   return generateResponse({ status, content });
 };
 
+const getContentReqResponse = (uri) => generateResponse(getContent(uri));
+
 const getResponse = (uri, verb, protocol) => {
-  if (isInvalidProtocol(protocol)) return getBadReqResponse();
-  if (isInvalidVerb(verb)) return getBadVerbResponse(verb);
+  if (isInvalidProtocol(protocol)) return getProtocolErrResponse();
+  if (isInvalidVerb(verb)) return getBadVerbErrResponse(verb);
   return getContentReqResponse(uri);
 };
 
@@ -69,6 +79,7 @@ const handleRequest = (socket, req) => {
 };
 
 const main = () => {
+  const PORT = 8000;
   const server = net.createServer();
 
   server.on('connection', (socket) => {
@@ -76,7 +87,6 @@ const main = () => {
     socket.on('data', (req) => handleRequest(socket, req));
   });
 
-  const PORT = 8000;
   server.listen(PORT, () => console.log('Listening on', PORT));
 };
 
