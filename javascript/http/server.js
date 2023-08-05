0f56c0e425910802b29fc604e9a627a6f930e770
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-05 13:02:40 +0530

    change put status code and message in status

--- javascript/http/server.js
+++ javascript/http/server.js
@@ -2,31 +2,32 @@ const net = require('node:net');
 
 const getContent = (uri) => {
   const contents = {
-    '/': { content: 'home', statusCode: 200, status: 'OK' },
-    '/ping': { content: 'pong', statusCode: 200, status: 'OK' },
-    '/echo': { content: 'echo', statusCode: 200, status: 'OK' },
+    '/': { content: 'home', status: { code: 200, message: 'OK' } },
+    '/ping': { content: 'pong', status: { code: 200, message: 'OK' } },
+    '/echo': { content: 'echo', status: { code: 200, message: 'OK' } },
   };
 
   const notFoundContent = {
-    statusCode: 404,
-    status: 'Not Found',
+    status: { message: 'Not Found', code: 404 },
     content: 'Not Found',
   };
 
   return contents[uri] || notFoundContent;
 };
 
-const generateResponse = (status, statusCode, content) =>
-  `HTTP/1.1 ${statusCode} ${status}\n\n ${content}`;
+const generateResponse = (status, content) =>
+  `HTTP/1.1 ${status.code} ${status.message}\n\n${content}`;
 
 const main = () => {
   const server = net.createServer();
 
   server.on('connection', (socket) => {
     socket.setEncoding('utf-8').on('data', (req) => {
+      console.log(req);
+
       const [, uri] = req.split(' ');
-      const { content, statusCode, status } = getContent(uri);
-      const response = generateResponse(status, statusCode, content);
+      const { content, status } = getContent(uri);
+      const response = generateResponse(status, content);
 
       socket.write(response);
       socket.end();
