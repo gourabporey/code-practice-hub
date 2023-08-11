7d1b6c70b8c270f125171b83c6d2b16f374ff544
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-11 13:12:50 +0530

    add redirection to wikipedia homepage on /wiki request

--- javascript/http-query-string/server.js
+++ javascript/http-query-string/server.js
@@ -3,16 +3,27 @@ const http = require('http');
 const parseParams = (queryString) =>
   Object.fromEntries(queryString.split('&').map((param) => param.split('=')));
 
+const handleSearch = (_, res, queryString) => {
+  const queryParams = parseParams(queryString);
+
+  res.write('\n');
+  res.write(`queryString ${JSON.stringify(queryParams)}`);
+};
+
+const redirectToWikipedia = (req, res) => {
+  res.writeHead(302, { location: 'https://www.wikipedia.org' });
+};
+
 const main = () => {
   const server = http.createServer((req, res) => {
     const [resource, queryString] = req.url.split('?');
 
     if (resource === '/search') {
-      const queryParams = parseParams(queryString);
+      handleSearch(req, res, queryString);
+    }
 
-      res.write(`resource ${resource}`);
-      res.write('\n');
-      res.write(`queryString ${JSON.stringify(queryParams)}`);
+    if (resource === '/wiki') {
+      redirectToWikipedia(req, res);
     }
 
     res.end();
