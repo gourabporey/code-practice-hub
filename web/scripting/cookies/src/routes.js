fa66b115178e403b17151be72d801a7417eac06f
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-18 00:25:30 +0530

    refactor logout handler

--- web/scripting/cookies/src/routes.js
+++ web/scripting/cookies/src/routes.js
@@ -12,36 +12,71 @@ const serveLoginPage = (req, res) => {
   });
 };
 
-const authenticateUser = (req, res) => {
-  let reqBody = '';
-  req.on('data', (data) => (reqBody += data));
-  req.on('end', () => {
-    const cookies = new URLSearchParams(reqBody);
-    const username = cookies.get('username');
-    res.setHeader('set-cookie', `username=${username}`);
-    res.statusCode = 302;
-    res.setHeader('location', '/');
-    res.end();
+const matchPassword = ({ username, password }, { onsuccess, onfailure }) => {
+  fs.readFile('./data/accounts.json', (err, data) => {
+    if (err) {
+      res.end(err.toString);
+      return;
+    }
+
+    let accounts = {};
+    try {
+      accounts = JSON.parse(data);
+    } catch {
+      accounts = {};
+    } finally {
+      const isMatch = accounts[username]?.password === password;
+      return isMatch ? onsuccess() : onfailure();
+    }
   });
 };
 
-const redirectToLogin = (req, res) => {
-  res.statusCode = 302;
-  res.setHeader('location', '/login');
-  res.end();
-};
-
-const redirectToHomePage = (req, res) => {
+const loginUser = (req, res) => {
+  res.setHeader('set-cookie', `username=${req.cookies.get('username')}`);
   res.statusCode = 302;
   res.setHeader('location', '/');
   res.end();
 };
 
-const logoutUser = (req, res) => {
-  const cookies = Object.entries(req.cookies).map(
-    ([name, value]) => `${name}=${value}; Max-Age=0`
-  );
-  res.setHeader('set-cookie', cookies);
+const handleInvalidCredentials = (_, res) => {
+  res.statusCode = 401;
+  res.write('Invalid credentials');
+  res.end();
+};
+
+const authenticateUser = (req, res) => {
+  let reqBody = '';
+  req.on('data', (data) => (reqBody += data));
+  req.on('end', () => {
+    req.cookies = new URLSearchParams(reqBody);
+
+    const username = req.cookies.get('username');
+    const password = req.cookies.get('password');
+
+    matchPassword(
+      { username, password },
+      {
+        onsuccess: () => loginUser(req, res),
+        onfailure: () => handleInvalidCredentials(req, res),
+      }
+    );
+  });
+};
+
+const redirectToLogin = (_, res) => {
+  res.statusCode = 302;
+  res.setHeader('location', '/login');
+  res.end();
+};
+
+const redirectToHomePage = (_, res) => {
+  res.statusCode = 302;
+  res.setHeader('location', '/');
+  res.end();
+};
+
+const logoutUser = (_, res) => {
+  res.setHeader('clear-site-data', '"cookies"');
   res.end();
 };
 
