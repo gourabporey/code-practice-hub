4aca70db359baa45c8a857daee5ba71dbd3367a2
Gourab Porey <gourabporey@users.noreply.github.com>
2023-08-30 14:09:07 +0530

    add read file promise

--- javascript/promises/index.js
+++ javascript/promises/index.js
@@ -1,1 +1,12 @@
+const fs = require('fs');
+
 const wait = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+const readFile = (filePath, encoding = 'utf-8') => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, encoding, (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+};
