6e66afbc7301ac8c130e6a25e009b27d009ef887
Gourab Porey <gourabporey@users.noreply.github.com>
2023-09-13 22:28:12 +0530

    add shuffle in lodash

--- javascript/lodash/src/array.js
+++ javascript/lodash/src/array.js
@@ -1,5 +1,7 @@
-const { chunk, compact } = require('lodash');
+const { chunk, compact, shuffle } = require('lodash');
 
 console.log(chunk([1, 2, 3, 4], 3));
 
 console.log(compact([1, 3, false, undefined, null, 7]));
+
+console.log(shuffle([1, 2, 3, 4]));
