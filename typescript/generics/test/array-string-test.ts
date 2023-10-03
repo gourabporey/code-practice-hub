8eac320d5b97d85a04109c3a6947e722fbf78bcf
Gourab Porey <gourabporey@users.noreply.github.com>
2023-10-03 16:17:54 +0530

    add tests for array and string functions

--- typescript/generics/test/array-string-test.ts
+++ typescript/generics/test/array-string-test.ts
@@ -1,5 +1,5 @@
 import assert from 'node:assert';
-import { reverse } from '../src/array-strings';
+import { concat, identity, reverse } from '../src/array-strings';
 import { describe, it } from 'node:test';
 
 describe('reverse', () => {
@@ -7,5 +7,19 @@ describe('reverse', () => {
     const actual = reverse([1, 2, 3]);
     const expected = [3, 2, 1];
     assert.deepStrictEqual(actual, expected);
+  });
+});
+
+describe('concat', () => {
+  it('should concat one string with another', () => {
+    const actual = concat<string>('gourab', 'porey');
+    const expected = 'gourabporey';
+    assert.deepStrictEqual(actual, expected);
+  });
+});
+
+describe('identity', () => {
+  it('should give the same element', () => {
+    assert.strictEqual(identity(5), 5);
   });
 });
