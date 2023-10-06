ae57161d3db5ba60218e2418173f4868dbe4c142
Gourab Porey <gourabporey@users.noreply.github.com>
2023-10-06 19:05:23 +0530

    Use graphics package in minefield practice game

---
+++ java/minefield/src/io/github/gourabporey/minefield/MinefieldGame.java
@@ -0,0 +1,10 @@
+package io.github.gourabporey.minefield;
+
+import io.github.gourabporey.graphics.shapes.Rectangle;
+
+class MinefieldGame {
+  public static void main(String[] args) {
+    Rectangle r = new Rectangle(5, 2);
+    System.out.println(r.area());
+  }
+}
