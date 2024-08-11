891e1c04e78b49f1116a3731eca6f9875f311031
Gourab Porey <gourabporey63@gmail.com>
2024-08-11 20:57:47 +0530

    Add two_sum solution

--- leetcode/rust/src/main.rs
+++ leetcode/rust/src/main.rs
@@ -2,6 +2,6 @@ use rust::two_sum::two_sum;
 
 fn main() {
   println!("Hello, world!");
-  let indexes = two_sum(vec![1, 2, 3, 4], 5);
+  let indexes = two_sum(vec![1, 2, 3, 8], 9);
   print!("[{}, {}]", indexes.get(0).unwrap(), indexes.get(1).unwrap());
 }
--- leetcode/rust/src/two_sum.rs
+++ leetcode/rust/src/two_sum.rs
@@ -1,19 +1,18 @@
 use std::collections::HashMap;
 
+fn parse_int(num_like: usize) -> i32 {
+  num_like.to_string().parse().unwrap()
+}
+
 pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-  let mut solution: Vec<i32> = Vec::new();
   let mut map: HashMap<i32, usize> = HashMap::new();
 
   for (index, num) in nums.iter().enumerate() {
-    if map.contains_key(num) {
-      match map.get(num) {
-        Some(x) => solution.push((*x).to_string().parse().unwrap()),
-        None => print!("Some error occurred"),
-      }
-      solution.push(index.to_string().parse().unwrap());
+    if let Some(x) = map.get(num) {
+      return vec![parse_int(*x), parse_int(index)];
     }
     map.insert(target - num, index);
   }
 
-  solution
+  vec![]
 }
