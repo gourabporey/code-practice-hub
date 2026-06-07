52824f870e66276af489f8cfb19e7db9bf1e35c2
Gourab Porey <gourabporey63@gmail.com>
2026-06-08 00:29:08 +0530

    Gitignore gradle related folders in Orm-Example

---
+++ kotlin/OrmExample/src/main/kotlin/com/example/OrmExample/OrmExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.OrmExample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrmExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<OrmExampleApplication>(*args)
+}
---
+++ kotlin/OrmExample/src/test/kotlin/com/example/OrmExample/OrmExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.OrmExample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrmExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
